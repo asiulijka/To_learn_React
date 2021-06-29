11621a0a1d90c72aa78514399f9413a057649127
asiulijka <j.m.h.zielinska@gmail.com>
2021-06-30 09:21:20 +1000

    Add Column - task 15.4 - props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,10 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
-        <List />
+        <List title={['Do things to', <sup key='1'>soon!</sup>]} 
+              image={'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2020%2F09%2F15%2FThe-Mandalorian5.jpg'} >
+          {/* <p>Sooner do things, later rather than!</p> */}
+        </List>
       </main>
     )
   }
