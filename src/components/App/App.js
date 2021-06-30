08b6c221335344a2b40c78a8c83ed1b9385868c3
asiulijka <j.m.h.zielinska@gmail.com>
2021-07-01 01:43:31 +1000

    Add states - part of 15.5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello world!</h2>
-        <List title={['Do things to', <sup key='1'>soon!</sup>]} 
-              image={'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2020%2F09%2F15%2FThe-Mandalorian5.jpg'} >
-          {/* <p>Sooner do things, later rather than!</p> */}
-        </List>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        <List {...listData} />
       </main>
     )
   }
